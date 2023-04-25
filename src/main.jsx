0e90694d5223a058f2053820691b383a2e6c4089
dhamithdev <dhamith@bluebaylabs.co>
2023-04-25 08:53:15 +0530

    changes in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { store } from './services/store.js';
-
+import { inject } from '@vercel/analytics';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,3 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>,
 )
+inject();
